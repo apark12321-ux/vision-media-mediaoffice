ef7e38808227171c6d919aae3bc52ce3fd6e8fa6
apark12321-ux <apark12321@gmail.com>
2026-06-17 16:58:41 +0900

    Add search page

---
+++ app/search/page.tsx
@@ -0,0 +1,3 @@
+export default function SearchPage() {
+  return <main className="mx-auto max-w-4xl px-4 py-12"><h1 className="text-4xl font-black text-brand-navy">기사검색</h1><p className="mt-4 text-gray-700">검색 기능 준비 중입니다.</p></main>;
+}
